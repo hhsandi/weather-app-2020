b5eb60317a34349e28ad08f63142274045fa5bcd
hhsandi <70669989+hhsandi@users.noreply.github.com>
2020-11-08 14:56:22 -0800

    updated icon

--- script.js
+++ script.js
@@ -103,8 +103,7 @@ let celsiusTemperature = null;
 
 function changeWeatherIcon(response) {
   let iconElement = document.querySelector("#icon");
-  iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
-  iconElement.setAttribute("alt", response.data.weather[0].description);
+  iconElement.innerHTML = `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`;
 }
 
 let searchForm = document.querySelector("#search-form");
