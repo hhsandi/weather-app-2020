9bcf3d63598600f4ab98162aaadf99db0f0620d4
hhsandi <70669989+hhsandi@users.noreply.github.com>
2020-11-08 11:38:01 -0800

    updating weather icon

--- script.js
+++ script.js
@@ -92,10 +92,13 @@ function convertToCelsius(event) {
   temperatureElement.innerHTML = 19;
 }
 
-function changeWeatherIcon(event) {
-  event.preventDeFault();
+function changeWeatherIcon(response) {
   let iconElement = document.querySelector("#icon");
-  iconElement.innerHTML = "http://openweatherap.org/img/wn/04d@2x.png";
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
 let searchForm = document.querySelector("#search-form");
